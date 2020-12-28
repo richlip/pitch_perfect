445d2261ed70baf71fb293b55d30d575f2a10b4f
richard <richlip@googlemail.com>
2020-12-28 20:04:51 +0100

    Initial Code

---
+++ PitchPerfect/PitchPerfect/PitchPerfectApp.swift
@@ -0,0 +1,17 @@
+//
+//  PitchPerfectApp.swift
+//  PitchPerfect
+//
+//  Created by Richard Lipski on 28.12.20.
+//
+
+import SwiftUI
+
+@main
+struct PitchPerfectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
